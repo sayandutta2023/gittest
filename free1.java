5c50a0a6a13ba4d31b0a9d2782660b9fe265847e
sayandutta2023 <sayan.dutta@agpaytech.co.uk>
2023-02-15 10:41:53 +0530

    commit

---
+++ free1.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class gittest {
+    
+    public static void main(String[] args)
+    {
+        int num = 188443;
+        if (num % 2 == 0) {
+            System.out.println("Entered Number is Even");
+        }
+        else 
+{
+            System.out.println("Entered Number is Odd");
+        }
+    }
+}
